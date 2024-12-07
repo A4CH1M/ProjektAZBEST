ba02d79ed6497cac90ab3a66933ac744ee01bb93
Weronika <pastusiakweronika@gmail.com>
2024-12-07 23:58:57 +0100

    dark mode + styles matching more with figma design

--- lepszy_plan/public/scripts/switches.js
+++ lepszy_plan/public/scripts/switches.js
@@ -5,4 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
     checkbox.addEventListener("change", () => {
         label.textContent = checkbox.checked ? "OR" : "AND";
     });
+
+    const darkModeSwitch = document.getElementById('dark-mode-switch');
+
+    darkModeSwitch.addEventListener('change', function () {
+        document.body.classList.toggle('dark-mode', this.checked);
+    });
 });
